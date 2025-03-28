8af88f93848cd03960a80b3ecd5cdddb8fc0e4cd
Giacomo Bertelli <itjustfordrive@gmail.com>
2025-03-28 11:23:40 +0100

    fixed issue with fonts

--- app/src/lib/App.svelte
+++ app/src/lib/App.svelte
@@ -1,39 +1,28 @@
 <script lang="ts">
-    import MathFieldv1 from "$lib/MathFieldv1.svelte";
     import MathFieldv2 from "$lib/MathFieldv2.svelte";
 
-    let value1 = $state('\\begin{pmatrix}\\placeholder[answer]{} & 2\\\\ 3 & 4\\end{pmatrix}');
-    let value2 = $state('\\begin{pmatrix}\\placeholder[answer00]{} & \\placeholder[answer01]{}\\\\ 3 & 4\\end{pmatrix}');
-    let answer1 = $state();
-	let mathEle1 = $state();
+    // let value = $state('\\displaystyle\\begin{bmatrix}\\placeholder[answer00]{1} & \\placeholder[answer01]{}\\\\ 3 & 4\\end{bmatrix}');
+    let value = $state('\\begin{bmatrix}\\placeholder[answer00]{1} & \\placeholder[answer01]{\\phantom{1}}\\\\ 3 & 4\\end{bmatrix}');
 
-    let prompts2 = $state();
+    let prompts = $state();
     // let getPromptValue2 = $state();
     let answer2 = $state();
-	$inspect(prompts2);
-    $effect(()=>{
-        value1 = value1;
-        answer1 = mathEle1.getPromptValue('answer');
-  })
+	$inspect(prompts);
 </script>
 
-<div>
-Version 1:
-<MathFieldv1 bind:value={value1} bind:node={mathEle1} read-only></MathFieldv1>
-<p>Current LaTeX: {value1}</p>
-
-<p>Prompt (answer):  {answer1}</p>
-</div>
 
 
 <div>
 Version 2:
-<MathFieldv2 bind:value={value2} bind:carlo={prompts2}  read-only></MathFieldv2>
-<p>Current LaTeX: {value2}</p>
+<MathFieldv2 bind:value={value} bind:prompts={prompts} smart-mode="true" default-mode="inline-math"></MathFieldv2>
+<p>Current LaTeX: {value}</p>
 
 <p>Prompt (answer):  {answer2}</p>
-<p>Prompts2:  {prompts2}</p>
+<p>Prompts2:  {prompts}</p>
 <p>Answer:  {answer2}</p>
+<button onclick={()=>{
+    prompts["answer00"] = "x+1";
+    }}>Set value [0,0] to "x+1"</button>
 </div>
 
 
--- app/src/lib/MathFieldv2.svelte
+++ app/src/lib/MathFieldv2.svelte
@@ -3,39 +3,49 @@
   import type { MathfieldElement, MathfieldElementAttributes } from "mathlive";
   import { onMount} from "svelte";
   import {on} from "svelte/events"
-
-  type Props = { value?: string } & Partial<MathfieldElementAttributes>;
-    type promptsDict = {[key: string]:string}
+  import "mathlive/fonts.css"
+// import {mathliveStyle} from "mathlive/fonts.css"    
+  type promptsDict = {[key: string]:string}
+  type Props = { value?: string, prompts?:promptsDict } & Partial<MathfieldElementAttributes>;
   let { value = $bindable(), 
-        carlo = $bindable(),
+        prompts = $bindable(),
         getPromptValue = $bindable(),
         ...rest }: Props = $props();
-    carlo = {} as promptsDict;
 
+  prompts = {};
   const init = (node: MathfieldElement) => {
     onMount(()=>{
-
-    for (let p of node.getPrompts()){
-        carlo[p] = "ciao";
-        console.log(p);
-    }
+        node.smartFence = true;
+        for (let p of node.getPrompts()){
+            prompts[p] = "ciao";
+            console.log(p);
+        }
     })
     $effect(() => {
         if (value) node.value = value;
-        //   console.log(value);
-        
+        if (prompts) {
+            for (let p in prompts) {
+                if (node.getPromptValue(p) != prompts[p]){
+                    node.setPromptValue(p, prompts[p], {})
+                }
+            }
+        } 
     });
     $effect(() => {
       return on(node, "input", () => {
         value = node.value;
         for (let p of node.getPrompts()) {
             // console.log(node.getPromptValue(p))
-            carlo[p] = node.getPromptValue(p);
+            prompts[p] = node.getPromptValue(p);
             // console.log(carlo);
         }
       });
     });
   };
 </script>
+<!-- <head>
+  <link rel="stylesheet" href="mathlive-fonts.css" />
+</head> -->
+<!-- <style> @import "node_modules\\mathlive\\mathlive-fonts.css" </style> -->
 
 <math-field use:init {...rest}></math-field>
